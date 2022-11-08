16ca09857e70a94db1f98ec147551c3fb7a8423d
Hasnat Mahbub <imhasnat00@gmail.com>
2022-11-08 11:43:55 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -11,6 +12,26 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server is running');
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ovbuiyj.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const services = client.db('serviceReview').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = services.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally { }
+}
+run().catch(err => console.log(err.message))
+
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
