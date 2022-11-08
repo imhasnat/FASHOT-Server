2f1483198023fa178318e55f0e32e45ede414594
Hasnat Mahbub <imhasnat00@gmail.com>
2022-11-08 21:32:05 +0600

    user review delete api added

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
             res.send(result);
 
         })
+
+        app.delete('/deletereview/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviews.deleteOne(query);
+            res.send(result);
+        })
     }
     finally { }
 }
